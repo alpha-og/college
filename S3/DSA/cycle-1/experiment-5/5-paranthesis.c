e7b0a1de75354eb2797d1cb3c79e5c2717f47daa
Athul Anoop <athulanoop2020@gmail.com>
2024-08-02 23:28:26 +0530

    incomplete code for 5th question in cycle 1

---
+++ S3/DSA/cycle-1/experiment-5/5-paranthesis.c
@@ -0,0 +1,118 @@
+/*
+ * Experiment - 05
+ * Program to
+ * Athul Anoop
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ValidChar {
+  char character;
+  struct ValidChar *next;
+} ValidChar;
+
+typedef struct ValidString {
+  ValidChar *content;
+  struct ValidString *next;
+} ValidString;
+
+ValidChar *get_last_valid_char(ValidChar *head) {
+  if (head->next == NULL)
+    return head;
+  else
+    return get_last_valid_char(head->next);
+}
+
+ValidChar *append_valid_char(char character, ValidChar *head) {
+  ValidChar *next = (ValidChar *)malloc(sizeof(ValidChar));
+  next->next = NULL;
+  next->character = character;
+  if (head == NULL)
+    head = next;
+  else
+    get_last_valid_char(head)->next = next;
+  return head;
+}
+
+void clear_valid_char(ValidChar *head) {
+  while (head->next != NULL) {
+    ValidChar *tmp = head;
+    while (tmp->next != NULL && tmp->next->next != NULL) {
+      tmp = tmp->next;
+    }
+    free(tmp->next);
+    tmp->next = NULL;
+  }
+  free(head);
+}
+
+void display_valid_string(ValidChar *head) {
+  printf("%c", head->character);
+  if (head->next == NULL) {
+    printf("\n");
+    return;
+  }
+  return display_valid_string(head->next);
+}
+
+ValidString *append_valid_string(ValidString *head) {
+  ValidString *next = (ValidString *)malloc(sizeof(ValidString));
+  next->next = NULL;
+  next->content = NULL;
+  if (head == NULL)
+    head = next;
+  else
+    head->next = next;
+  return head;
+}
+
+ValidString *get_last_valid_string(ValidString *head) {
+  if (head->next == NULL)
+    return head;
+  else
+    return get_last_valid_string(head->next);
+}
+ValidString *pop_valid_string(ValidString *head) {
+  ValidString *tmp = head;
+  while (tmp->next != NULL && tmp->next->next != NULL) {
+    tmp = tmp->next;
+  }
+  free(tmp->next);
+  tmp->next = NULL;
+  return tmp;
+}
+int main() {
+  printf("Provide some text: ");
+  char character = '\0';
+  int inside = 0;
+  int is_empty = 1;
+  ValidString *valid_string_head = NULL;
+  ValidChar *valid_char_head = NULL;
+  while ((character = fgetc(stdin)) != '\n') {
+    if (character == '(') {
+      inside = 1;
+      if (is_empty == 1) {
+        valid_string_head = append_valid_string(NULL);
+        is_empty = 0;
+      } else {
+        append_valid_string(get_last_valid_string(valid_string_head));
+      }
+      valid_string_head->content = (ValidChar *)malloc(sizeof(ValidChar));
+      valid_char_head = valid_string_head->content;
+      continue;
+    } else if (character == ')' && inside == 1) {
+      inside = 0;
+      ValidString *tail = get_last_valid_string(valid_string_head);
+      ValidChar *tail_content = tail->content;
+      display_valid_string(tail_content);
+      clear_valid_char(tail_content);
+      valid_string_head = pop_valid_string(valid_string_head);
+      continue;
+    }
+    if (inside == 1) {
+      valid_char_head = append_valid_char(character, valid_char_head);
+    }
+  }
+  return 0;
+}
