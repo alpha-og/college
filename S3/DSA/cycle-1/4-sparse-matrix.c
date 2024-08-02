b04a500ce96aebe55dcff7ba21052b946664831a
Athul Anoop <athulanoop2020@gmail.com>
2024-08-02 08:21:38 +0530

    incomplete code for processing sparse matrix using tuples

--- S3/DSA/cycle-1/4-sparse-matrix.c
+++ S3/DSA/cycle-1/4-sparse-matrix.c
@@ -1,3 +1,9 @@
+/*
+ * Experiment - 04
+ * Program to store sparse matrices in tuple form and perform operations on them
+ * Athul Anoop
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,14 +28,25 @@ typedef struct Matrix {
   MatrixElement *elements;
 } Matrix;
 
-void create_matrix_element(Matrix *matrix) {
+MatrixElement *get_tail(MatrixElement *current_element) {
+  if (current_element->next == NULL) {
+    return current_element;
+  } else {
+    return get_tail(current_element->next);
+  }
+}
+
+void create_matrix_element(Matrix *matrix, Position position, int value) {
   MatrixElement *matrix_element =
       (MatrixElement *)malloc(sizeof(MatrixElement));
+  matrix_element->next = NULL;
+  matrix_element->position = position;
+  matrix_element->value = value;
   if (matrix->elements == NULL) {
     matrix->elements = matrix_element;
   } else {
-    matrix_element->next = matrix->elements;
-    matrix->elements = matrix_element;
+    MatrixElement *tail = get_tail(matrix->elements);
+    tail->next = matrix_element;
   }
 }
 
@@ -40,6 +57,20 @@ Size read_size() {
   return size;
 }
 
+void read_elements(Matrix *matrix) {
+  printf("Provide the matrix elements row by row (x1, x2, ...)\n");
+  for (int row = 0; row < matrix->size.rows; row++) {
+    for (int column = 0; column < matrix->size.columns; column++) {
+      int element = 0;
+      scanf("%d,", &element);
+      if (element != 0) {
+        Position position = {.row = row, .column = column};
+        create_matrix_element(matrix, position, element);
+      }
+    }
+  }
+}
+
 Matrix *create_matrix(Size size) {
   Matrix *matrix = (Matrix *)malloc(sizeof(Matrix));
   matrix->size = size;
@@ -47,4 +78,44 @@ Matrix *create_matrix(Size size) {
   return matrix;
 }
 
-int main() { return 0; }
+void display_matrix_elements(Matrix *matrix, Position *current_position,
+                             MatrixElement *current_element) {
+  int inserted = 0;
+  for (int i = 0; i < matrix->size.rows; i++) {
+    if (inserted == 1)
+      break;
+    for (int j = 0; j < matrix->size.columns; j++) {
+      if (j == 0)
+        printf("[");
+      if (current_element->position.row == i &&
+          current_element->position.column == j) {
+        printf("%d, ", current_element->value);
+        inserted = 1;
+        current_position->row = i;
+        current_position->column = j;
+
+        break;
+      }
+      if (j == (matrix->size.columns - 1))
+        printf("],\n");
+    }
+  }
+  if (current_element->next == NULL)
+    return;
+  return display_matrix_elements(matrix, current_position,
+                                 current_element->next);
+}
+
+void display_matrix(Matrix *matrix) {
+  printf("[");
+  Position current_position = {.row = 0, .column = 0};
+  display_matrix_elements(matrix, &current_position, matrix->elements);
+  printf("]");
+}
+
+int main() {
+  Matrix *matrix = create_matrix(read_size());
+  read_elements(matrix);
+  display_matrix(matrix);
+  return 0;
+}
