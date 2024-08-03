8c68dec840d7923af5b22cc08a2b44c89ac2cb82
Athul Anoop <athulanoop2020@gmail.com>
2024-08-03 06:47:24 +0530

    fixed issues processing multiple nested paranthesis

--- S3/DSA/cycle-1/experiment-5/5-paranthesis.c
+++ S3/DSA/cycle-1/experiment-5/5-paranthesis.c
@@ -88,36 +88,38 @@ int main() {
   int inside = 0;
   int is_empty = 1;
   ValidString *valid_string_head = NULL;
-  ValidChar *valid_char_head = NULL;
   while ((character = fgetc(stdin)) != '\n') {
     if (character == '(') {
       inside = 1;
       if (is_empty == 1) {
         valid_string_head = append_valid_string(NULL);
         is_empty = 0;
+
       } else {
-        valid_string_head =
-            append_valid_string(get_last_valid_string(valid_string_head));
+        append_valid_string(get_last_valid_string(valid_string_head));
       }
-      valid_string_head->content = (ValidChar *)malloc(sizeof(ValidChar));
-      valid_char_head = valid_string_head->content;
       continue;
     } else if (character == ')' && inside == 1) {
-      ValidString *tail = get_last_valid_string(valid_string_head);
-      ValidChar *tail_content = tail->content;
-      display_valid_string(tail_content);
-      clear_valid_char(tail_content);
       if (valid_string_head->next == NULL) {
         inside = 0;
+        display_valid_string(valid_string_head->content);
+        clear_valid_char(valid_string_head->content);
         free(valid_string_head);
         valid_string_head = NULL;
-				is_empty = 1;
-      } else
-        valid_string_head = pop_valid_string(valid_string_head);
-      continue;
+        is_empty = 1;
+      } else {
+        ValidString *tail = get_last_valid_string(valid_string_head);
+        ValidChar *tail_content = tail->content;
+        display_valid_string(tail_content);
+        clear_valid_char(tail_content);
+
+        pop_valid_string(valid_string_head);
+        continue;
+      }
     }
     if (inside == 1) {
-      valid_char_head = append_valid_char(character, valid_char_head);
+      get_last_valid_string(valid_string_head)->content = append_valid_char(
+          character, get_last_valid_string(valid_string_head)->content);
     }
   }
   return 0;
