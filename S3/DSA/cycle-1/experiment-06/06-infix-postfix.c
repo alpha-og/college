944b813de1e8d8b1db660363b1e636be8bdec782
Athul Anoop <athulanoop2020@gmail.com>
2024-08-18 14:57:35 +0530

    completed question 6

--- S3/DSA/cycle-1/experiment-06/06-infix-postfix.c
+++ S3/DSA/cycle-1/experiment-06/06-infix-postfix.c
@@ -11,18 +11,19 @@
 #define MAX 100
 
 void display_menu();
-void handle_choice(char *expression);
-int precedence(char operand);
-void convert_infix_to_postfix(char *expression);
+void handle_choice(char **expression);
 void push(char *end, char **top, char ch);
 char pop(char *start, char **top);
-// void evaluate_postfix(char *expression);
+int precedence(char operand);
+void convert_infix_to_postfix(char **expression);
+void evaluate_postfix(char **expression);
+void operate(int *index, int *tokens, char operator);
 
 int main() {
-  char expression[MAX] = {'\0'};
+  char *expression = (char *)calloc(MAX, sizeof(char));
   while (1) {
     display_menu();
-    handle_choice(expression);
+    handle_choice(&expression);
   }
 }
 
@@ -34,7 +35,7 @@ void display_menu() {
   printf("Choice: ");
 }
 
-void handle_choice(char *expression) {
+void handle_choice(char **expression) {
   int choice;
   scanf("%d", &choice);
   switch (choice) {
@@ -42,7 +43,7 @@ void handle_choice(char *expression) {
     convert_infix_to_postfix(expression);
     break;
   case 2:
-    // evaluate_postfix(expression);
+    evaluate_postfix(expression);
     break;
   case 3:
     printf("\nExiting...\n");
@@ -73,26 +74,29 @@ char pop(char *start, char **top) {
   return tmp;
 }
 
-void convert_infix_to_postfix(char *expression) {
+void convert_infix_to_postfix(char **expression) {
   char tmp[MAX] = {'\0'};
   char ch = {'\0'};
   char *topTmp = tmp;
-  char *topExp = expression;
+  *expression = calloc(MAX, sizeof(char));
+  char *topExp = *expression;
   int isTerm = 0;
   getchar();
   while (ch != '\n') {
     ch = getchar();
     if (ch >= '0' && ch <= '9') {
       if (isTerm == 0) {
-        push(expression + MAX, &topExp, ' ');
+        if (*topExp != '\0') {
+          push(*expression + MAX, &topExp, ' ');
+        }
         isTerm = 1;
       }
-      push(expression + MAX, &topExp, ch);
+      push(*expression + MAX, &topExp, ch);
     } else if (ch == ')') {
       isTerm = 0;
       while (*topTmp != '(') {
-        push(expression + MAX, &topExp, ' ');
-        push(expression + MAX, &topExp, pop(tmp, &topTmp));
+        push(*expression + MAX, &topExp, ' ');
+        push(*expression + MAX, &topExp, pop(tmp, &topTmp));
       }
       pop(tmp, &topTmp);
     } else if (ch == '(') {
@@ -100,21 +104,72 @@ void convert_infix_to_postfix(char *expression) {
       push(tmp + MAX, &topTmp, ch);
     } else if (ch == '\n') {
       while (topTmp != tmp) {
-        push(expression + MAX, &topExp, ' ');
-        push(expression + MAX, &topExp, pop(tmp, &topTmp));
+        push(*expression + MAX, &topExp, ' ');
+        push(*expression + MAX, &topExp, pop(tmp, &topTmp));
       }
-      push(expression + MAX, &topExp, ' ');
-      push(expression + MAX, &topExp, pop(tmp, &topTmp));
+      push(*expression + MAX, &topExp, ' ');
+      push(*expression + MAX, &topExp, pop(tmp, &topTmp));
     } else {
       isTerm = 0;
       while (*topTmp != '\0' && precedence(ch) <= precedence(*topTmp)) {
-        push(expression + MAX, &topExp, ' ');
-        push(expression + MAX, &topExp, pop(tmp, &topTmp));
+        push(*expression + MAX, &topExp, ' ');
+        push(*expression + MAX, &topExp, pop(tmp, &topTmp));
       }
       push(tmp + MAX, &topTmp, ch);
     }
   }
-  printf("Final Expression: %s\n", expression);
+  printf("Final Expression: %s\n", *expression);
+}
+
+void evaluate_postfix(char **expression) {
+  int *tokens = (int *)calloc(MAX, sizeof(int));
+  int index = -1;
+  char *current = *expression;
+  while (*current != '\0') {
+    if (*current >= '0' && *current <= '9') {
+      char token[MAX] = {'\0'};
+      char *topToken = token;
+      while (*current != ' ') {
+        push(token + MAX, &topToken, *current);
+        current = current + 1;
+      }
+      index++;
+      sscanf(token, "%d", &tokens[index]);
+    } else if (*current == ' ') {
+      current = current + 1;
+      continue;
+    } else {
+
+      operate(&index, tokens, *current);
+      current = current + 1;
+    }
+  }
+  printf("Evaluated Expression: %d\n", tokens[0]);
+}
+
+void operate(int *index, int *tokens, char operator) {
+  int result = 0;
+  switch (operator) {
+  case '-':
+    result = tokens[*index - 1] - tokens[*index];
+    break;
+  case '+':
+    result = tokens[*index - 1] + tokens[*index];
+    break;
+  case '*':
+    result = tokens[*index - 1] * tokens[*index];
+    break;
+  case '/':
+    result = tokens[*index - 1] / tokens[*index];
+    break;
+  default:
+    fprintf(stderr, "ERROR: Invalid operator! Exiting...");
+    exit(1);
+    break;
+  }
+  tokens[*index] = 0;
+  *index = *index - 1;
+  tokens[*index] = result;
 }
 
 int precedence(char operand) {
