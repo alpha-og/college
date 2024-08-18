c12e7ff0e2dc6c3b96720563ce8449c63f5a99de
Athul Anoop <athulanoop2020@gmail.com>
2024-08-18 22:19:29 +0530

    code files for question 8 and 9

---
+++ S3/DSA/cycle-1/experiment-08/08-relief.c
@@ -0,0 +1,14 @@
+/*
+ * In a relief camp food packets are supplied to persons based on the following
+ * conditions. Only 3 packets are supplied to a person at a time. He will be
+ * supplied with tokens according to the number of persons in his family. Only
+ * one person from a family is allowed in the queue. There can be a maximum of
+ * only N persons in the queue at any time. A person can be allowed to enter the
+ * end of the queue if he has to collect more packets. Simulate these conditions
+ * using a circular queue with the following operations:
+ * 1. Allow a new person at the end of the queue, if the queue is not full.
+ * 2. Number of persons in the queue.
+ * 3. Display all persons in the queue with the number of tokens they have.
+ * 4. Supplying food to the person in front of the queue.
+ * Athul Anoop
+ */
---
+++ S3/DSA/cycle-1/experiment-09/09-DEQUEUE.c
@@ -0,0 +1,13 @@
+/*
+ * Experiment - 09
+ * Write a menu driven program to implement a Double-Ended Queue (DEQUEUE). with
+ * the following operations. The elements of the queue are strings. Wherever you
+ * are deleting an entry from the queue, display whether that string is
+ * palindrome or not.
+ * 1. Insert elements to the front of the queue.
+ * 2. Insert elements to the rear of the queue
+ * 3. Delete elements from the Front of the queue.
+ * 4. Delete elements from the Rear of the queue.
+ * 5. Display the contents of the queue.
+ * Athul Anoop
+ */
