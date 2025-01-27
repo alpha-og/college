33772537c753071141dece506816f6970693fa92
Athul Anoop <athulanoop2020@gmail.com>
2025-01-27 22:34:54 +0530

    update: added problem details

--- S4/os_lab/cycle-1/p11/p11.c
+++ S4/os_lab/cycle-1/p11/p11.c
@@ -1,3 +1,17 @@
+/*
+ * Problem Number: 11
+ *
+ * Description
+ * Create a new process using the fork() system call. Have the processes
+ * introduce themselves with their process IDs as follows: Parent: I am <myID>,
+ * and my child is <mychildID> Child: I am <myID>, and my parent is <myparentID>
+ * Use the pstree command to display the process tree for the child process
+ * starting from the init process. The child process is to be “highlighted” in
+ * the tree.
+ *
+ * Athul Anoop
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
