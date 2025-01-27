3b34d28cf37b9a9530c335a153a90837a2647fe6
Athul Anoop <athulanoop2020@gmail.com>
2025-01-27 22:32:55 +0530

    update: sleep added to allow for monitoring process tree

--- S4/os_lab/cycle-1/p11/p11.c
+++ S4/os_lab/cycle-1/p11/p11.c
@@ -11,8 +11,10 @@ int main() {
 
   if (pid > 0) {
     printf("I am %d, and my child is %d\n", getpid(), pid);
+    wait(NULL);
   } else {
     printf("I am %d, and my parent is %d\n", getpid(), getppid());
+    sleep(10);
   }
   return 0;
 }
