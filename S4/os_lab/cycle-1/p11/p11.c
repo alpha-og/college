01ad477e7d2c4ce155cdd706afd7fbcdf95b0631
Athul Anoop <athulanoop2020@gmail.com>
2025-01-27 22:25:57 +0530

    add: completed code for question 11
    need to use pstree on the process to obtain process tree of the child
    process starting from the init process

---
+++ S4/os_lab/cycle-1/p11/p11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+  pid_t pid = fork();
+  if (pid < 0) {
+    printf("Failed to fork\n");
+  }
+
+  if (pid > 0) {
+    printf("I am %d, and my child is %d\n", getpid(), pid);
+  } else {
+    printf("I am %d, and my parent is %d\n", getpid(), getppid());
+  }
+  return 0;
+}
