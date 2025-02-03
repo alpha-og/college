1f4feb2a2b5c5ee3442b5839bebf4eb5c2fa5b85
Athul Anoop <athulanoop2020@gmail.com>
2025-02-03 07:56:31 +0530

    update:

---
+++ S4/os_lab/cycle-1/p9/p9.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Problem Number: 9
+#
+# Description:
+# Create a menu driven script to print the following process information:
+# (a) Number of processes forked since the last boot
+# (b) Number of processes currently in the system
+# (c) Number of running processes
+# (d) Number of blocked processes
+# (e) PID of the current shell
+# (f) Number of context switches performed by this shell. How many of these were forcibly taken?
