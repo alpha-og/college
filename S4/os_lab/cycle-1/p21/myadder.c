3b7f21a832f5e5df684f8d55a84081d98df0c73b
Athul Anoop <athulanoop2020@gmail.com>
2025-03-04 06:48:11 +0530

    add: completed p21

---
+++ S4/os_lab/cycle-1/p21/myadder.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *args[]) {
+  int a = atoi(args[1]);
+  int b = atoi(args[2]);
+  printf("a + b = %d\n", a + b);
+  return 0;
+}
---
+++ S4/os_lab/cycle-1/p21/p21.c
@@ -0,0 +1,23 @@
+// Athul Anoop
+// Roll no: 25
+// Description: C program to fork a new process and load a custom binary into
+// the child process
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  pid_t pid = fork();
+
+  if (pid < 0) {
+    perror("\nFork failed");
+    return 0;
+  } else if (pid == 0) {
+    char *args[] = {"./myadder", argv[1], argv[2], NULL};
+    execvp("./myadder", args);
+  } else {
+    wait(NULL);
+    return 0;
+  }
+}
