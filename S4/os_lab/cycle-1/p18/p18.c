00723520482aa4bc2a882f4ce63fd90996f0979b
Athul Anoop <athulanoop2020@gmail.com>
2025-03-10 19:48:42 +0530

    update: include system date and time output

--- S4/os_lab/cycle-1/p18/p18.c
+++ S4/os_lab/cycle-1/p18/p18.c
@@ -6,8 +6,17 @@
 
 #include <stdio.h>
 #include <sys/resource.h>
+#include <time.h>
 
 int main() {
+  time_t t;
+  time(&t);
+  struct tm *time = localtime(&t);
+
+  char buffer[100];
+  strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", time);
+  printf("Current system date and time: %s\n", buffer);
+
   struct rusage usage;
   if (getrusage(RUSAGE_SELF, &usage) == 0) {
     printf("User mode CPU time: %ld.%06ld seconds\n", usage.ru_utime.tv_sec,
