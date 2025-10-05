4ed1c0e5439480d99e4b2a74cb11c71517630046
Athul Anoop <athulanoop2020@gmail.com>
2025-10-05 07:57:54 +0530

    add: completed cycle 4 and 5

---
+++ s5/dbms/cycle_4.sql
@@ -0,0 +1,73 @@
+-- 1. a) Revoke insert privilege for a user on table Students and check whether you are able to insert a row in to the table
+--    b) Grant the permission to the user for inserting values in to students table and check whether insertion is possible or not
+REVOKE INSERT ON students FROM s23b25;
+INSERT INTO students (studfirstname, studlastname, studstreetadress, studcity, studstate, studzipcode, studareacode, studphonenumber) VALUES
+    ('Vijay', 'Kumar', 'elm Street', 'Lucknow', 'Uttar Pradesh', 861537, 851, '5988804556');
+GRANT INSERT ON students TO s23b25;
+
+
+-- 2. a) Start a new transaction and insert a row into the Staff table. Commit the transaction and display the changes to the table
+-- b) Start a new transaction and insert a row into the Staff table.Undo the transaction and display the changes to the table
+-- START TRANSACTION
+INSERT INTO staff (	
+	stfFirstName,	
+	stfLastName,	
+	stfStreetAdress,	
+	stfCity,	
+	stfState,	
+	stfZipCode,	
+	stfAreaCode,	
+	stfPhoneNumber,	
+	joiningdate,	
+	salary,	
+	position
+) VALUES ('Ajay', 'Kumar', 'elm street', 'Chennai', 'Tamilnadu', 618800, 02, '1234567790', '2024-06-17', 70000, 'Professor');
+COMMIT;
+
+START TRANSACTION
+INSERT INTO staff (	
+	stfFirstName,	
+	stfLastName,	
+	stfStreetAdress,	
+	stfCity,	
+	stfState,	
+	stfZipCode,	
+	stfAreaCode,	
+	stfPhoneNumber,	
+	joiningdate,	
+	salary,	
+	position
+) VALUES	
+('Ravi', 'Shankar', 'hay street', 'Chennai', 'Tamilnadu', 618800, 02, '1234567890', '2024-01-27', 40000, 'Teaching Assistant');
+ROLLBACK;
+
+-- 3. Display the staffid and title for Faculty along with staffid and position for Staff in a single table. Indicate the source of the row in the result by adding an additional column EMPLOYEE with possible values as ‘F’ (Faculties) and ‘S’ (Staff). Display all rows (Using UNION ALL)
+SELECT 'F' as Source, faculty.staffid, title FROM faculty UNION ALL (SELECT 'S' as Source, staff.staffid, position FROM staff);
+
+-- 4. Find the pass percentage of a particular subject (using grade)
+SELECT 
+    S.SubjectName,
+    ROUND(
+        100.0 * SUM(CASE WHEN SS.Grade > 30  THEN 1 ELSE 0 END) / COUNT(*), 2
+    ) AS PassPercentage
+FROM STUDENT_SCHEDULES SS
+JOIN CLASSES C ON SS.ClassID = C.ClassID
+JOIN SUBJECTS S ON C.SubjectID = S.SubjectID
+WHERE S.SubjectName = 'VLSI Design'
+group by s.subjectname;
+
+-- 5. Display the number of students in each classroom on a particular building using JOINS
+SELECT buildings.buildingcode, classes.classroomid, COUNT(DISTINCT student_schedules.studentid) FROM buildings JOIN classrooms ON buildings.buildingcode = classrooms.buildingcode JOIN classes ON classrooms.classroomid = classes.classroomid JOIN student_schedules ON classes.classid = student_schedules.classid GROUP BY buildings.buildingcode, classes.classroomid;
+
+-- 6. Display the list of students and staff who have the same zip code
+SELECT S1.StudFirstName, S1.StudLastName, ST1.StfFirstName, ST1.StfLastName, S1.StudZipCode
+FROM STUDENTS S1
+JOIN STAFF ST1 ON S1.StudZipCode = ST1.stfzipcode;
+
+-- 7. Display the list of faculty who engage same subject (for any particular subject name)
+SELECT F.StaffID, F.Title, S.SubjectName
+FROM FACULTY_SUBJECTS FS
+JOIN SUBJECTS S ON FS.SubjectID = S.SubjectID
+JOIN FACULTY F ON FS.StaffID = F.StaffID
+WHERE S.SubjectName = 'VLSI Design';
+
---
+++ s5/dbms/cycle_5.sql
@@ -0,0 +1,81 @@
+-- 1. Create a view from faculty, staff and student relation to display faculty name,
+-- classid, subject name based on schedule given.
+CREATE VIEW view_faculty AS
+  SELECT staff.stffirstname || ' ' || staff.stfLastName, faculty_classes.classid, subjects.subjectname
+  FROM faculty
+  JOIN staff ON faculty.staffid = staff.staffid
+  JOIN faculty_classes ON faculty.staffid = faculty_classes.staffid
+  JOIN faculty_subjects ON faculty.staffid = faculty_subjects.staffid
+  JOIN subjects ON faculty_subjects.subjectid = subjects.subjectid;
+
+-- 2. Use assert statement to check whether Classroom table has at least 1 data
+DO $$
+DECLARE
+  count NUMERIC;
+BEGIN
+  SELECT count(*) INTO count FROM classroom;
+  ASSERT count > 0, 'Classroom table is empty';
+END $$;
+
+-- 3. Write a PL/SQL block to do the following:
+-- a. Using control structures, display the list of faculties who have been hired
+-- between two dates
+-- b. Using CASE statement, satisfy the following conditions
+-- accepting Staff ID as input:
+-- i. Case 1: if salary < 5000, give 50% increment
+-- ii. Case 2: if salary between 5000 and 10000, give 10% increment 
+-- iii. Case 3: if salary > 10000, display a message ‘No Increment’ 
+-- c. Read the Faculty table row by row using WHILE loop and display the last name of
+-- the faculty (Example: ‘Last Name: Antony’)
+
+DO $$
+DECLARE 
+  v_start DATE:='2020-01-01';
+  v_end DATE:='2022-12-31';
+  rec RECORD;
+BEGIN
+  FOR rec IN SELECT stffirstname ||' '|| stflastname AS faculty_name
+    FROM staff
+    WHERE joiningdate BETWEEN v_start AND v_end
+    loop raise notice 'Faculty:%',rec.faculty_name;
+  END loop;
+END $$;
+
+DO $$
+DECLARE 
+  v_staffid int:=13;
+  v_salary NUMERIC;
+BEGIN
+  SELECT salary INTO v_salary
+  FROM staff 
+  WHERE staffid=v_staffid;
+  CASE WHEN v_salary<5000 THEN
+    UPDATE staff
+    SET salary=salary*1.5
+    WHERE staffid=v_staffid;
+    RAISE NOTICE 'salary incremented by 50%%';
+    WHEN v_salary BETWEEN 5000 AND 10000 THEN
+    UPDATE staff
+    SET salary=salary*1.1
+    WHERE staffid=v_staffid;
+    RAISE NOTICE 'salary incremented by 10%%';
+    ELSE raise notice 'no increment';
+    END CASE;
+END $$;
+
+DO $$
+DECLARE 
+rec RECORD;
+cur CURSOR FOR SELECT stflastname FROM faculty f
+JOIN staff s ON f.staffid=s.staffid;
+BEGIN
+  OPEN cur;
+  FETCH cur INTO rec;
+  WHILE FOUND LOOP
+    RAISE NOTICE 'Last Name: %', rec.stflastname;
+    FETCH cur INTO rec;
+  END LOOP;
+  CLOSE cur;
+END $$;
+
+
